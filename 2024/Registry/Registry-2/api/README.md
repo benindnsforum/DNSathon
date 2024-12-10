cf73e85828c4b38afc5fc70927caafed62ee8fb7
Spéro ADONON <spro.adonon@gmail.com>
2024-12-11 00:59:01 +0100

    Update resolver 2024 folder

--- 2024/Registry/Registry-2/api/README.md
+++
@@ -1,3 +0,0 @@
-# DNS tld benin api
-A very small API that handle basic feature related to a Registy.
-
--- 2024/Registry/Registry-2/api/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from api import views
-
-urlpatterns = [
-    path('domain/verify/', views.verify_domain),
-    path('domain/create/', views.create_domain),
-]
